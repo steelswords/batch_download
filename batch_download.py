47c0cd008b3007ba69d4d53f49e85544c38a55a6
Tristan Andrus <tristan.andrus@aggiemail.usu.edu>
2021-04-25 22:39:05 -0600

    Fix bug
    Other code cleanup
    
    Signed-off-by: Tristan Andrus <tristan.andrus@aggiemail.usu.edu>

--- batch_download.py
+++ batch_download.py
@@ -8,9 +8,10 @@ import os
 #PROJECT MANAGEMENT
 # Features to add:
 # X Strip URL and save as URL name file
-# - Command line options
+# X Command line options
 # - Parallel downloads
 # - Progress bars
+# - Logins so I can download from Canvas, etc.
 
 # Set up command line options
 parser = OptionParser()
@@ -20,9 +21,10 @@ parser.add_option("-e", "--extension", dest="extensions", action="append", defau
         help="The extension (without the leading .) you want the script to download")
 parser.add_option("-u", "--url", dest="websiteUrl", action="store",
         help="The URL you want to scrape")
+(options, args) = parser.parse_args()
 
 def getWebsiteRoot(websiteUrl, relativeUrl):
-    # Returns the root of the website in case the URL is relative, not absolute
+    # Makes a relative URL an absolute one given the base page URL.
     rootEnd = websiteUrl.rfind('/')
     websiteRoot = websiteUrl[0 : rootEnd + 1]
     relative = relativeUrl
@@ -33,9 +35,7 @@ def getWebsiteRoot(websiteUrl, relativeUrl):
         
     return websiteRoot + relative
 
-(options, args) = parser.parse_args()
 
-#websiteUrl="http://classes.ece.usu.edu/3640/resources.html"
 websiteUrl = options.websiteUrl
 extensions = options.extensions
 
@@ -48,18 +48,12 @@ soup = BeautifulSoup(website.content, 'html.parser')
 
 allAnchors = soup.find_all('a')
 for a in allAnchors:
-    #print(a)
-
     allDownloads = []
-    #extension='pdf'
     for extension in extensions:
         allAnchors = soup.find_all('a')
         for a in allAnchors:
-            #print(a.string)
             if 'href' in a.attrs:
-                #print(a.attrs['href'])
                 matches = re.findall(r".*\.%s.*" %extension, a.attrs['href'])
-                #print(matches)
                 if matches:
                     allDownloads.append(matches[0])
 
@@ -69,15 +63,17 @@ print("-----------------------------------------------------")
 print("Files to download:")
 print("-----------------------------------------------------")
 for url in allDownloads:
-    if (url[0:3] != "http"):
+    if (url[0:4] != "http"):
         url = getWebsiteRoot(websiteUrl, url)
     print(url)
     beginningOfFileName = url.rfind('/')
     fileName = url[beginningOfFileName + 1 : len(url)]
+
     # Remove anything after the extension
     endOfExtension = fileName.find(extension)
     fileName = fileName[0:endOfExtension + len(extension)]
     fileName = os.path.join(options.outputDir, fileName)
+
     print("Downloading %s...." %fileName, end='')
     file = requests.get(url);
     print("Done. Saving...", end='')
